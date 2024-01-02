728479ba66a71d0e0a9f01a6aabc43ecb2f63114
Liam Beguin <liambeguin@gmail.com>
2024-01-02 10:55:38 -0500

    tools: add helper to generate an archive
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

---
+++ tools/archive.py
@@ -0,0 +1,26 @@
+import argparse
+import sigmf
+from pathlib import Path
+
+
+def main():
+    parser = argparse.ArgumentParser('sig-archive')
+    parser.add_argument('metafile', type=Path)
+    parser.add_argument('--outputdir', '-o', type=Path, default='.')
+    options = parser.parse_args()
+
+    arc = options.outputdir / (options.metafile.stem + '.sigmf')
+
+    if not options.metafile.exists():
+        print('input file doesn\'t exist')
+        return
+
+    meta = sigmf.sigmffile.fromfile(options.metafile.as_posix())
+
+    with open(arc, 'wb') as out:
+        meta.archive(fileobj=out)
+    print(f'wrote: {arc}')
+
+
+if __name__ == '__main__':
+    main()
